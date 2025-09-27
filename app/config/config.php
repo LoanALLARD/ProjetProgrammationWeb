98ebebe45188381cf18bc2b91d486cc7f970348e
Loan <loan.allard@etu.univ-amu.fr>
2025-09-27 20:35:59 +0200

    Commentaires pour config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,12 @@
 <?php
 return [
+    // MySQL information
     'db_host' => 'mysql-devweb-iutaix.alwaysdata.net',
     'db_name' => 'devweb-iutaix_programation_web',
     'db_user' => '429892',
     'db_pass' => '^E&I8KFKD%mF7k',
 
+    // SMTP information
     'smtp_host' => 'smtp-devweb-iutaix.alwaysdata.net',
     'smtp_port' => 465,
     'smtp_user' => 'devweb-iutaix@alwaysdata.net',

45b2eb4a879f2c224f553bedb3b9e826cd12e798
LoanALLARD <loan.allard@etu.univ-amu.fr>
2025-09-27 21:54:58 +0200

    Commentaires pour Router

--- app/core/Router.php
+++ app/core/Router.php
@@ -3,10 +3,16 @@
 namespace core;
 class Router {
     public function run() {
+        // if $_GET['url'] is not null, use it, else use home/index
         $url = $_GET['url'] ?? 'home/index';
+
+        // Cut the string $url with /
+        // => $params = []
         $params = explode('/', $url);
 
+        // Build the name of the controller to instantiate
         $controllerName = 'controllers\\' . ucfirst($params[0]) . 'Controller';
+        // $methosName = $param[1] if exist, else, $methodName = index
         $methodName = $params[1] ?? 'index';
 
         if (class_exists($controllerName)) {

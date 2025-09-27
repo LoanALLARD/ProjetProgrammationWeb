7e5541dd7f3a23f494afea8f50e75270acb3c427
Loan <loan.allard@etu.univ-amu.fr>
2025-09-27 21:34:53 +0200

    Ajout commentaires pour SitemapController

--- app/controllers/SitemapController.php
+++ app/controllers/SitemapController.php
@@ -3,16 +3,21 @@ namespace controllers;
 
 class SitemapController {
 
+    // Array to store the different pages
     private $pages = [];
 
+    // Getter : getPages
     public function getPages() {
         return $this->pages;
     }
 
     public function index() {
+        // Stores files matching the pattern in an array
         $controllerFiles = glob(__DIR__ . '/*.php');
 
+        // Browse the array $controllerFiles
         foreach ($controllerFiles as $file) {
+            // Sets $fileName relative to the controller name
             $filename = basename($file, '.php');
 
             // Ignore SitemapController
@@ -20,9 +25,12 @@ class SitemapController {
                 continue;
             }
 
+            // Rename the pages by removing “controller”
             $pageName = str_replace('Controller', '', $filename);
+
             $url = "/index.php?url=" . strtolower($pageName) . "/index";
 
+            // Stores the page and URL in the array
             $this->pages[] = [
                 'title' => $pageName,
                 'url'   => $url
@@ -32,12 +40,14 @@ class SitemapController {
         $pages = $this->getPages();
         $pageTitle = "Plan du site";
 
+        // Directory of the view
         $viewPathSiteMap = __DIR__ . '/../views/siteMap.php';
+
+        // Try to display the view
         if (file_exists($viewPathSiteMap)) {
             require $viewPathSiteMap;
         } else {
-            echo "Erreur lors du chargement de la vue siteMap.php";
+            echo "Erreur lors du chargement de la vue " . $viewPathSiteMap;
         }
     }
 }
-?>

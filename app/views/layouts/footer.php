6a1c2c7bc68b6d60c1dd5826c83c280a611e1bae
Loan <loan.allard@etu.univ-amu.fr>
2025-10-08 14:11:58 +0200

    Ajout réseaux sociaux dans le footer

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,6 +1,13 @@
     <footer>
-        <p id="footerText">Copyright &copy; <?= date('Y') ?> Evan - Ewan - Loan</p>
-        <a href="/index.php?url=mentionsLegales" id="sitemapLink">Mentions Legales</a>
+        <div>
+            <p id="footerText">Copyright &copy; <?= date('Y') ?> Evan - Ewan - Loan</p>
+            <a href="/index.php?url=mentionsLegales" id="sitemapLink">Mentions légales</a>
+        </div>
+        <div id="footerIconsPart">
+            <a class="footerIcons" href="https://www.instagram.com/mmnotes_officiel/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            <a class="footerIcons" href="https://x.com/mmnotesofficiel?s=21" target="_blank"><i class="fa-brands fa-x-twitter" target="_blank"></i></a>
+            <a class="footerIcons" href="https://www.mickael-martin-nevot.com/" target="_blank"><i class="fa-solid fa-laptop-code" target="_blank"></i></a>
+        </div>
     </footer>
     <script src="https://kit.fontawesome.com/242e672808.js" crossorigin="anonymous"></script>
 </body>
